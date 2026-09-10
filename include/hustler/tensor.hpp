bfc9fbbe4cd13d4e0bffae3e083868693c9f8eb3
Viet Nguyen <zi3t@users.noreply.github.com>
2026-09-10 10:26:38 +1000

    abstract

--- include/hustler/tensor.hpp
+++ include/hustler/tensor.hpp
@@ -1,1 +1,14 @@
 #pragma once
+
+#include <cstddef>
+#include <vector>
+
+class Tensor {
+private:
+  std::vector<float> values;
+  std::vector<std::size_t> shape;
+  std::vector<std::size_t> computed_strides;
+
+public:
+  Tensor(std::vector<float> values, std::vector<std::size_t> shape);
+};
