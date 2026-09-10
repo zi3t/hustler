837961db0befbbe7dff3fa501ae775e556f8ae1d
Viet Nguyen <zi3t@users.noreply.github.com>
2026-09-10 17:18:28 +1000

    simple start

--- src/tensor.cpp
+++ src/tensor.cpp
@@ -1,1 +1,15 @@
-#include "../include/hustler/tensor.hpp"
+#include "hustler/tensor.hpp"
+#include <stdexcept>
+#include <utility>
+
+Tensor::Tensor(std::vector<float> input_values,
+               std::vector<std::size_t> input_shape)
+    : values(std::move(input_values)), shape(std::move(input_shape)) {
+  std::size_t count = 1;
+  for (std::size_t dimension : shape) {
+    count *= dimension;
+  }
+  if (count != values.size()) {
+    throw std::invalid_argument("value count does not match shape");
+  }
+}
